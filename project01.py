e975ea155690f71a365d20cf092dc337b45fe302
Maheen Mohsin <maheenmohsin644@gmail.com>
2025-09-27 02:05:13 +0500

    Create project01.py

---
+++ project01.py
@@ -0,0 +1,11 @@
+#Project 1: Hello World + User Input Form
+
+print("Hello Wworld")
+
+name = input("What is your name?")
+
+age = input("How old are you?")
+
+print("Nice to meet you,", name + "You are", age, "years old")
+
+print("Did you Know? In 5 years, you will be", int(age) + 5, "years old!")
